a05432c53b231908c15204f94bc667e1c3f1b807
miguel-lebit <miguel.lebit@escola.pr.gov.br>
2026-09-02 15:00:24 -0300

    atualizei

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+const numeros = Math.floor(Math.randon() * desafios.length);
+    const desafio = desafios[numeros];
 
+    document.getElementById("resposta").innerHTML=`
+    <h2> Olá ${name}</h2>`
+    `<h2> Seu Desafio é: ${desafio}</h2>`
