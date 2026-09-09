55279c261f49c84ca0b6ffbeb96f2eff1818dc50
miguel-lebit <miguel.lebit@escola.pr.gov.br>
2026-09-09 15:02:57 -0300

    ghh

--- main.js
+++ main.js
@@ -1,6 +1,53 @@
-const numeros = Math.floor(Math.randon() * desafios.length);
-    const desafio = desafios[numeros];
+const desafios = [
+    "Resolver um problema lógico",
+    "Criar diferentes soluções",
+    "Aprender novas funções",
+    "Descobrir padrões",
+    "Pensar em uma nova invenção"
+];
 
-    document.getElementById("resposta").innerHTML=`
-    <h2> Olá ${name}</h2>`
-    `<h2> Seu Desafio é: ${desafio}</h2>`
+function avaliarResposta(){
+    const name = document.getElementById("name").value;
+    const resposta = document.getElementById("resposta").value;
+    const textoDesafio = document.getElementById("# resultado h3").innerHTML;
+
+    if (resposta.trim() === ""){
+        alert("digite a resposta primeiro antes de proseguir")   
+        return;
+    }
+
+    let pontos = 0;
+
+    if (resposta.length >= 30){
+        pontos += 30;
+    }
+
+    const texto = resposta.toLowerCase();
+    
+    if( texto.include("criar")||
+        texto.include("desenvolver")||
+        texto.include("elaborar") ){
+        pontos += 30;
+    }
+    if( texto.include("pesquisa")||
+        texto.include("estudo")||
+        texto.include("projeto") ){
+        pontos += 30;
+    }
+
+    const tempo = Math.floor(Math.randon() * 10)+1;
+
+    let nivel;
+
+    if (pontos >= 90) {
+    nivel = "Inventor de ideias";
+} 
+
+else if (pontos >= 50) {
+    nivel = "Desenvolvedor de ideias";
+}
+
+else if (pontos >= 50) {
+    nivel = "Desenvolvedor de ideias";
+
+}
